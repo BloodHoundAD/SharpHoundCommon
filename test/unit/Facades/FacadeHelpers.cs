4d9ccd25ac29e44d11c01c6b38024a9049c1a2aa
Rohan Vazarkar <rvazarkr@gmail.com>
2021-06-08 13:16:15 -0400

    Create FacadeHelpers to help build mockable stuff

---
+++ test/unit/Facades/FacadeHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace CommonLibTest.Facades
+{
+    public class FacadeHelpers
+    {
+        const BindingFlags nonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+        const BindingFlags publicInstance = BindingFlags.Public | BindingFlags.Instance;
+        
+        internal static T GetUninitializedObject<T>()
+        {
+            return (T) FormatterServices.GetUninitializedObject(typeof(T));
+        }
+        
+        internal static void SetProperty<T1, T2>(T1 obj, string propertyName, T2 propertyValue)
+        {
+            var set = typeof(T1).GetField(propertyName, nonPublicInstance);
+            set.SetValue(obj, propertyValue);
+        }
+    }
+}
