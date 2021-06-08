1816da40d4d202478c760fef8092c31662002033
Rohan Vazarkar <rvazarkr@gmail.com>
2021-06-08 13:16:25 -0400

    Add MockableForest

---
+++ test/unit/Facades/MockableForest.cs
@@ -0,0 +1,15 @@
+﻿using System.DirectoryServices.ActiveDirectory;
+
+namespace CommonLibTest.Facades
+{
+    public class MockableForest
+    {
+        public static Forest Construct()
+        {
+            var forest = FacadeHelpers.GetUninitializedObject<Forest>();
+            FacadeHelpers.SetProperty(forest, "_forestDnsName", "PARENT.LOCAL");
+
+            return forest;
+        }
+    }
+}
