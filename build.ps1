3a37d96989a33379ca2dbe8bee621d3666f39373
0x13d <ari@thirteendelta.com>
2021-06-13 09:50:47 -0400

    move docfx out of root and update build.ps1

--- build.ps1
+++ build.ps1
@@ -21,16 +21,16 @@ nuget restore src\CommonLib\SharpHoundCommonLib.csproj
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput=build\coverage.json test\unit\CommonLibTest.csproj
 
 # generate test report from coverage.json. place directly in the docs folder to prevent copying a full static site needlessly
-reportgenerator "-reports:test\unit\build\coverage.json" "-targetdir:docs\coverage" -reporttypes:HTML
+reportgenerator "-reports:test\unit\build\coverage.json" "-targetdir:docs\articles\coverage" -reporttypes:HTML
 
 # performance tests are not implemented but this command will run the basic DotnetBenchmark example
 # dotnet run --project test\performance\CommonLibPerformance.csproj -c Release
 
 # build docs 
-# docfx
+# docfx ./docfx/docfx.json -o ./ -s
 
 # serve docs: to test documentation locally combines a build and local web server step into one call
-docfx --serve
+ddocfx ./docfx/docfx.json -o ./ -s
 
 # use act: requires docker
 # act -s GITHUB_TOKEN=[insert token or leave blank for secure input]
