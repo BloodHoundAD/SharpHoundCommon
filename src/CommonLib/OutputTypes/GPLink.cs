ade7ae8c7c3c76e6717bed562083f534a6bc5d11
Rohan Vazarkar <rvazarkr@gmail.com>
2021-06-17 13:13:04 -0400

    Add equality for GPLink

--- src/CommonLib/OutputTypes/GPLink.cs
+++ src/CommonLib/OutputTypes/GPLink.cs
@@ -6,5 +6,26 @@
         
         public bool IsEnforced { get; set; }
         public string GUID { get => _guid; set => _guid = value?.ToUpper(); }
+
+        protected bool Equals(GPLink other)
+        {
+            return _guid == other._guid && IsEnforced == other.IsEnforced;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((GPLink) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((_guid != null ? _guid.GetHashCode() : 0) * 397) ^ IsEnforced.GetHashCode();
+            }
+        }
     }
 }
