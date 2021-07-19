49be304fc0c2e8289ae1361953418435e2ccc28d
Rohan Vazarkar <rvazarkr@gmail.com>
2021-07-19 14:25:58 -0400

    Fix consistency for Domain vs OU objects Links property

--- src/CommonLib/OutputTypes/Domain.cs
+++ src/CommonLib/OutputTypes/Domain.cs
@@ -4,6 +4,6 @@
     {
         public TypedPrincipal[] ChildObjects { get; set; } = new TypedPrincipal[0];
         public DomainTrust[] Trusts { get; set; } = new DomainTrust[0];
-        public GPLink[] GPLinks { get; set; } = new GPLink[0];
+        public GPLink[] Links { get; set; } = new GPLink[0];
     }
 }
