b6a865f6d5f5c495638c41fb15f1fa7583ed476b
Rohan Vazarkar <rvazarkr@gmail.com>
2021-11-03 17:55:28 -0400

    Use Array.Empty

--- src/CommonLib/OutputTypes/SessionAPIResult.cs
+++ src/CommonLib/OutputTypes/SessionAPIResult.cs
@@ -1,7 +1,9 @@
-﻿namespace SharpHoundCommonLib.OutputTypes
+﻿using System;
+
+namespace SharpHoundCommonLib.OutputTypes
 {
     public class SessionAPIResult : APIResult
     {
-        public Session[] Results { get; set; } = new Session[0];
+        public Session[] Results { get; set; } = Array.Empty<Session>();
     }
 }
