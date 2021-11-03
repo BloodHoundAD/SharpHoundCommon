714760effebecd8f21c2c937c7317f0c95c072a4
Rohan Vazarkar <rvazarkr@gmail.com>
2021-11-03 17:55:37 -0400

    Add missing datatype

--- src/CommonLib/Enums/DataType.cs
+++ src/CommonLib/Enums/DataType.cs
@@ -4,7 +4,7 @@
     {
         public const string Users = "users";
         public const string Groups = "groups";
-
+        public const string Computers = "computers";
         public const string Domains = "domains";
         public const string GPOs = "gpos";
         public const string OUs = "ous";
