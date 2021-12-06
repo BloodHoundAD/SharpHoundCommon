61ff345a05993e0a492af0604023e7b9a7863e23
Rohan Vazarkar <rvazarkr@gmail.com>
2021-12-06 11:02:46 -0500

    Fix case on SPNTargets

--- src/CommonLib/OutputTypes/User.cs
+++ src/CommonLib/OutputTypes/User.cs
@@ -5,6 +5,6 @@
         public TypedPrincipal[] AllowedToDelegate { get; set; } = new TypedPrincipal[0];
         public string PrimaryGroupSID { get; set; }
         public TypedPrincipal[] HasSIDHistory { get; set; } = new TypedPrincipal[0];
-        public SPNTarget[] SpnTargets { get; set; } = new SPNTarget[0];
+        public SPNTarget[] SPNTargets { get; set; } = new SPNTarget[0];
     }
 }
