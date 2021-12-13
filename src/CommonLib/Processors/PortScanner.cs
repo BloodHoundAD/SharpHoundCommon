8fcd0d4d87ccc7022e86dfc8133296d98d43581a
Rohan Vazarkar <rvazarkr@gmail.com>
2021-12-13 11:22:10 -0500

    Add ping cache to PortScanner

--- src/CommonLib/Processors/PortScanner.cs
+++ src/CommonLib/Processors/PortScanner.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -7,6 +8,7 @@ namespace SharpHoundCommonLib.Processors
     public class PortScanner
     {
         private readonly ILogger _log;
+        private static readonly ConcurrentDictionary<PingCacheKey, bool> PingCache = new();
 
         public PortScanner()
         {
@@ -27,19 +29,70 @@ namespace SharpHoundCommonLib.Processors
         /// <returns>True if port is open, otherwise false</returns>
         public virtual async Task<bool> CheckPort(string hostname, int port = 445, int timeout = 500)
         {
+            var key = new PingCacheKey
+            {
+                Port = port,
+                HostName = hostname
+            };
+
+            if (PingCache.TryGetValue(key, out var status))
+            {
+                _log.LogTrace("Ping cache hit for {HostName} on {Port}: {Status}", hostname, port, status);
+                return status;
+            }
+            
             try
             {
                 using var client = new TcpClient();
                 var ca = client.ConnectAsync(hostname, port);
                 await Task.WhenAny(ca, Task.Delay(timeout));
                 client.Close();
-                if (!ca.IsFaulted && ca.IsCompleted) return true;
+                if (!ca.IsFaulted && ca.IsCompleted)
+                {
+                    PingCache.TryAdd(key, true);
+                    return true;
+                }
+                
                 _log.LogDebug("{Hostname} did not respond to ping", hostname);
+                PingCache.TryAdd(key, false);
                 return false;
             }
             catch
             {
+                PingCache.TryAdd(key, false);
                 return false;
+            }
+        }
+
+        public static void ClearCache()
+        {
+            PingCache.Clear();
+        }
+        
+        private class PingCacheKey
+        {
+            internal string HostName { get; set; }
+            internal int Port { get; set; }
+
+            protected bool Equals(PingCacheKey other)
+            {
+                return HostName == other.HostName && Port == other.Port;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((PingCacheKey)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return (HostName.GetHashCode() * 397) ^ Port;
+                }
             }
         }
     }
