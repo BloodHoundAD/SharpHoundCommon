d157efb060670efb952f928b39b465559c650946
Jonas Knudsen <jknudsen@specterops.io>
2023-05-13 22:19:22 -0700

    add CertTemplate OutputBase

---
+++ src/CommonLib/OutputTypes/CertTemplate.cs
@@ -0,0 +1,9 @@
+﻿using SharpHoundCommonLib.Processors;
+
+namespace SharpHoundCommonLib.OutputTypes
+{
+    public class CertTemplate : OutputBase
+    {
+        // TODO: Add CertTemplate stuff 
+    }
+}
