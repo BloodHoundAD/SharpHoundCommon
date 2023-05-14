d4064c23b2b71b17041812b13bed0c8cc5674532
Jonas Knudsen <jknudsen@specterops.io>
2023-05-13 22:06:01 -0700

    fix: Configuration NC CN correction

--- src/CommonLib/Enums/DirectoryPaths.cs
+++ src/CommonLib/Enums/DirectoryPaths.cs
@@ -7,6 +7,6 @@
         public const string CertTemplateLocation = "CN=Certificate Templates,CN=Public Key Services,CN=Services";
         public const string NTAuthCertificateLocation = "CN=NTAuthCertificates,CN=Public Key Services,CN=Services";
         public const string PKILocation = "CN=Public Key Services,CN=Services,CN=Configuration";
-        public const string ConfigLocation = "CN=Config";
+        public const string ConfigLocation = "CN=Configuration";
     }
 }
