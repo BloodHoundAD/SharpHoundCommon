88971acf73e7b4e0c349a8bf15bcd64be9db1d46
Jonas Knudsen <jknudsen@specterops.io>
2023-05-24 11:32:13 -0700

    Add CN=Configuration to CA paths

--- src/CommonLib/Enums/DirectoryPaths.cs
+++ src/CommonLib/Enums/DirectoryPaths.cs
@@ -2,10 +2,10 @@
 {
     public class DirectoryPaths
     {
-        public const string EnterpriseCALocation = "CN=Enrollment Services,CN=Public Key Services,CN=Services";
-        public const string RootCALocation = "CN=Certification Authorities,CN=Public Key Services,CN=Services";
-        public const string CertTemplateLocation = "CN=Certificate Templates,CN=Public Key Services,CN=Services";
-        public const string NTAuthCertificateLocation = "CN=NTAuthCertificates,CN=Public Key Services,CN=Services";
+        public const string EnterpriseCALocation = "CN=Enrollment Services,CN=Public Key Services,CN=Services,CN=Configuration";
+        public const string RootCALocation = "CN=Certification Authorities,CN=Public Key Services,CN=Services,CN=Configuration";
+        public const string CertTemplateLocation = "CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration";
+        public const string NTAuthCertificateLocation = "CN=NTAuthCertificates,CN=Public Key Services,CN=Services,CN=Configuration";
         public const string PKILocation = "CN=Public Key Services,CN=Services,CN=Configuration";
         public const string ConfigLocation = "CN=Configuration";
     }
