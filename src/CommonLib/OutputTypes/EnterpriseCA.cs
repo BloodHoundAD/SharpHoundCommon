a05c24a3d4e97f4d757e3f351161e27eb8fdc695
Jonas Knudsen <jknudsen@specterops.io>
2023-10-12 06:55:24 -0700

    remove DomainSID from EnterpriseCA

--- src/CommonLib/OutputTypes/EnterpriseCA.cs
+++ src/CommonLib/OutputTypes/EnterpriseCA.cs
@@ -2,7 +2,6 @@
 {
     public class EnterpriseCA : OutputBase
     {
-        public string DomainSID { get; set; }
         public string HostingComputer { get; set; }
         public CARegistryData CARegistryData { get; set; }
         public TypedPrincipal[] EnabledCertTemplates { get; set; }
