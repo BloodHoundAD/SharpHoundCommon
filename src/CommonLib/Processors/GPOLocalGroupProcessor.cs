f5f191a82abd54ef027c7ff1530fe3493f6c87f9
nurfed1 <bryandehouwer@hotmail.com>
2024-01-19 21:06:31 +0100

    Check if ResolveAccountName returns a value (#90)

--- src/CommonLib/Processors/GPOLocalGroupProcessor.cs
+++ src/CommonLib/Processors/GPOLocalGroupProcessor.cs
@@ -531,6 +531,11 @@ namespace SharpHoundCommonLib.Processors
                                     var domain = s[0];
 
                                     var res = _utils.ResolveAccountName(name, domain);
+                                    if (res == null)
+                                    {
+                                        _log.LogWarning("Failed to resolve member {memberName}", memberName);
+                                        continue;
+                                    }
                                     ga.Target = GroupActionTarget.LocalGroup;
                                     ga.TargetSid = res.ObjectIdentifier;
                                     ga.TargetType = res.ObjectType;
@@ -540,6 +545,11 @@ namespace SharpHoundCommonLib.Processors
                                 else
                                 {
                                     var res = _utils.ResolveAccountName(memberName, gpoDomain);
+                                    if (res == null)
+                                    {
+                                        _log.LogWarning("Failed to resolve member {memberName}", memberName);
+                                        continue;
+                                    }
                                     ga.Target = GroupActionTarget.LocalGroup;
                                     ga.TargetSid = res.ObjectIdentifier;
                                     ga.TargetType = res.ObjectType;
