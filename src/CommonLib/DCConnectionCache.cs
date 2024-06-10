2b2ef57ea93a99a41c8a8d934d6b39e4f2a2501a
Alex Nemeth <80649445+definitelynotagoblin@users.noreply.github.com>
2024-06-10 13:11:09 -0700

    DC Connection Cache Breakout (#129)
    * Breaking dc connection cache out of utils
    
    * Normalize cache key
    
    * Correct domainSid cache key in utils
    
    * Remove unused property Server from LdapConnectionCacheKey

---
+++ src/CommonLib/DCConnectionCache.cs
@@ -0,0 +1,79 @@
+using System.Collections.Concurrent;
+using System.DirectoryServices.Protocols;
+
+namespace SharpHoundCommonLib
+{
+    public class DCConnectionCache
+    {
+        private readonly ConcurrentDictionary<LDAPConnectionCacheKey, LdapConnection> _ldapConnectionCache;
+
+        public DCConnectionCache()
+        {
+            _ldapConnectionCache = new ConcurrentDictionary<LDAPConnectionCacheKey, LdapConnection>();
+        }
+
+        public bool TryGet(string key, bool isGlobalCatalog, out LdapConnection connection)
+        {
+            var cacheKey = GetKey(key, isGlobalCatalog);
+            return _ldapConnectionCache.TryGetValue(cacheKey, out connection);
+        }
+
+        public LdapConnection AddOrUpdate(string key, bool isGlobalCatalog, LdapConnection connection)
+        {
+            var cacheKey = GetKey(key, isGlobalCatalog);
+            return _ldapConnectionCache.AddOrUpdate(cacheKey, connection, (_, existingConnection) =>
+            {
+                existingConnection.Dispose();
+                return connection;
+            });
+        }
+
+        public LdapConnection TryAdd(string key, bool isGlobalCatalog, LdapConnection connection)
+        {
+            var cacheKey = GetKey(key, isGlobalCatalog);
+            return _ldapConnectionCache.AddOrUpdate(cacheKey, connection, (_, existingConnection) =>
+            {
+                connection.Dispose();
+                return existingConnection;
+            });
+        }
+
+        private LDAPConnectionCacheKey GetKey(string key, bool isGlobalCatalog)
+        {
+            return new LDAPConnectionCacheKey(key.ToUpper().Trim(), isGlobalCatalog);
+        }
+
+        private class LDAPConnectionCacheKey
+        {
+            public string Domain { get; }
+            public bool GlobalCatalog { get; }
+
+            public LDAPConnectionCacheKey(string domain, bool globalCatalog)
+            {
+                GlobalCatalog = globalCatalog;
+                Domain = domain;
+            }
+
+            protected bool Equals(LDAPConnectionCacheKey other)
+            {
+                return GlobalCatalog == other.GlobalCatalog && Domain == other.Domain;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((LDAPConnectionCacheKey)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return (GlobalCatalog.GetHashCode() * 397) ^ (Domain != null ? Domain.GetHashCode() : 0);
+                }
+            }
+        }
+    }
+}
