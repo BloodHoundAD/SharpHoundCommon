2a3b1dd7aef86cdf5fec531f523f5aa84ffb20aa
rvazarkar <rvazarkar@specterops.io>
2024-07-15 16:22:37 -0400

    feat: add inheritancehashes property to containers/ous/domains

--- src/CommonLib/OutputTypes/Domain.cs
+++ src/CommonLib/OutputTypes/Domain.cs
@@ -8,5 +8,6 @@ namespace SharpHoundCommonLib.OutputTypes
         public TypedPrincipal[] ChildObjects { get; set; } = Array.Empty<TypedPrincipal>();
         public DomainTrust[] Trusts { get; set; } = Array.Empty<DomainTrust>();
         public GPLink[] Links { get; set; } = Array.Empty<GPLink>();
+        public string[] InheritanceHashes { get; set; } = Array.Empty<string>();
     }
 }
