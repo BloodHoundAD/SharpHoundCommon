87429b35bc5da9bc0f994655be1b033e00476755
rvazarkar <rvazarkar@specterops.io>
2024-07-23 12:01:11 -0400

    chore: add some tests covering new async enumerables

---
+++ test/unit/AsyncEnumerableTests.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using SharpHoundCommonLib;
+using Xunit;
+
+namespace CommonLibTest;
+
+public class AsyncEnumerableTests {
+    [Fact]
+    public async Task AsyncEnumerable_DefaultIfEmpty_Empty() {
+        var enumerable = AsyncEnumerable.Empty<int>().DefaultIfEmpty(1);
+        var e = enumerable.GetAsyncEnumerator();
+        var res = await e.MoveNextAsync();
+        Assert.True(res);
+        Assert.Equal(1, e.Current);
+        Assert.False(await e.MoveNextAsync());
+    }
+
+    [Fact]
+    public async Task AsyncEnumerable_FirstOrDefault() {
+        var enumerable = AsyncEnumerable.Empty<int>();
+        var res = await enumerable.FirstOrDefaultAsync();
+        Assert.Equal(0, res);
+    }
+
+    [Fact]
+    public async Task AsyncEnumerable_CombinedOperators() {
+        var enumerable = AsyncEnumerable.Empty<string>();
+        var res = await enumerable.DefaultIfEmpty("abc").FirstOrDefaultAsync();
+        Assert.Equal("abc", res);
+    }
+}
