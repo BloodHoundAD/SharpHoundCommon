44fa7923a0addfb802afbe67854ea8659cd6d4c0
rvazarkar <rvazarkar@specterops.io>
2024-08-07 14:30:17 -0400

    chore: remove unused import

--- src/CommonLib/ConnectionPoolManager.cs
+++ src/CommonLib/ConnectionPoolManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.DirectoryServices;
-using System.Runtime.CompilerServices;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
